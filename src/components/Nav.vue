6e65255b0f1933ea28cb8d74daa7f8b0199538b0
lucillem1313 <https://github.com/lucillem1313>
2022-03-01 19:10:33 -0600

    Basic setup

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -13,23 +13,23 @@ const loggingOut = () => {
 </script>
 
 <template>
-<div class="bg-purple-400 text-indigo-500">
+<div class="bg-red-400 text-gray-500">
 <div class="container flex items-center  justify-between mx-auto">  
 
 <h1 class=" tracking-tighter text-5xl font-thick">
-Vue<span class="font-semibold text-gray-600">FakeAuth</span>
+Vue<span class="font-semibold text-gray-600">RealAuth</span>
 </h1>
 <nav>
 <ul class="flex space-x-4"><router-link to="/">
 
-<li class="py-8 px-4 hover:cursor-pointer  hover:bg-indigo-300 hover:text-orange-500"> Home</li></router-link> 
-<router-link to="/About"><li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-300 hover:text-orange-500"> About</li></router-link> 
-<router-link v-if="!isAuthenticated" to="/Login"><li class="bg-gray-400 px-4 py-8 hover:cursor-pointer hover:bg-indigo-300 hover:text-red-500"> Login</li></router-link> 
+<li class="py-8 px-4 hover:cursor-pointer  hover:bg-gray-400 hover:text-orange-500"> Home</li></router-link> 
+<router-link to="/About"><li class="px-4 py-8 hover:cursor-pointer hover:bg-gray-400 hover:text-orange-500"> About</li></router-link> 
+<router-link v-if="!isAuthenticated" to="/Login"><li class="px-4 py-8 hover:cursor-pointer hover:bg-gray-400 hover:text-red-500"> Login</li></router-link> 
 <div v-else class="flex">
-<router-link  to="/Secret"><li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-300 hover:text-red-500"> Secret</li></router-link> 
+<router-link  to="/Secret"><li class="px-4 py-8 hover:cursor-pointer hover:bg-gray-400 hover:text-red-500"> Secret</li></router-link> 
 
 <button  @click="loggingOut">
-<li class=" px-6 py-8 hover:cursor-pointer hover:bg-indigo-300 hover:text-red-500"> Logout</li></button>
+<li class=" px-6 py-8 hover:cursor-pointer hover:bg-gray-400 hover:text-red-500"> Logout</li></button>
 
 
 </div>
