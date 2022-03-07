1e4252f73f0c698a04b15a1f2041a43dd8afa432
lucillem1313 <https://github.com/lucillem1313>
2022-03-06 18:40:51 -0600

    firestore

--- src/composable/useFirebase.js
+++ src/composable/useFirebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from 'firebase/firestore'
 import { firebaseConfig } from "../firebaseConfig";
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
 
-
+export const db = getFirestore()
 
   
